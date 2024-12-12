1c2f6e53f6da9184f6fc772c80b613b15098a63b
zzhbrr <zzhbrr@gmail.com>
2024-12-12 14:15:58 +0800

    fix

--- modeling.py
+++ modeling.py
@@ -5,7 +5,7 @@ Operations = ["Q", "K", "V", "qk_matmul", "softmax", "sv_matmul", "O", "FFN_up",
 weight_size = {}
 
 # Hardware Spec
-hardware = { # H100
+hardware = { # H100, 3TB/s，989 TFLOPS
     "bandwidth": 3072e9, # bytes/s 
     "FP16": 1979e12 / 2 # OPs/s
 }
@@ -14,7 +14,7 @@ def record(stage, operation, OPs, load_weight, load_act, store_act, load_kv_cach
     t = max((load_weight + load_act + store_act + load_kv_cache + store_kv_cache) / hardware["bandwidth"], OPs / hardware["FP16"])
     result[stage][operation] = {
         "name": operation,
-        "time": t*1e6,
+        "time": t*1e6, # us
         "OPs": OPs,
         "load_weight": load_weight,
         "load_act": load_act,
@@ -25,10 +25,11 @@ def record(stage, operation, OPs, load_weight, load_act, store_act, load_kv_cach
     weight_size[operation] = load_weight
     
 # Workload Spec
-batchsize = 10 # bachsize
-seql = 1024 # sequence length
+batchsize = 100 # bachsize
+seql = 102 # sequence length
 
 # Model Spec
+model_name = "Llama2-70B" 
 d = 8192 # hidden dimension
 d_ffn = 28672 # feedforward dimension
 h = 64 # number of attention heads
@@ -38,9 +39,49 @@ w_byte = 2
 kv_byte = 2
 headsize = d // h
 
+def ModelSpec(model_name):
+    global d, d_ffn, h, h_kv, a_byte, w_byte, kv_byte, headsize
+    if model_name == "Llama2-70B":
+        d = 8192 
+        d_ffn = 28672 
+        h = 64 
+        h_kv = 8 
+        a_byte = 2
+        w_byte = 2
+        kv_byte = 2
+        headsize = d // h
+    elif model_name == "OPT-30B":
+        d = 7168
+        d_ffn = 28672
+        h = 56
+        h_kv = 56
+        a_byte = 2
+        w_byte = 2
+        kv_byte = 2
+    else:
+        raise ValueError("Invalid model name")
+
+def PrintConfig():
+    print("Configuration: {")
+    print("Hardware: ")
+    print("    bandwidth: ", hardware["bandwidth"])
+    print("    FP16: ", hardware["FP16"])
+    print("Workload: ")
+    print("    batchsize: ", batchsize)
+    print("    seql: ", seql)
+    print("Model: ")
+    print("    model_name: ", model_name)
+    print("    d: ", d)
+    print("    d_ffn: ", d_ffn)
+    print("    h: ", h)
+    print("    h_kv: ", h_kv)
+    print("    a_byte: ", a_byte)
+    print("    w_byte: ", w_byte)
+    print("    kv_byte: ", kv_byte)
+    print("    headsize: ", headsize)
+    print("}")  
 
 def analyze():
-
     for op in Operations:
         if op == "Q":
             record("prefill", 
@@ -179,14 +220,17 @@ def analyze():
                    load_kv_cache=0, 
                    store_kv_cache=0)
 
-def GetKVCacheSize(): # bytes
+def GetKVCacheSizePerLayer(): # bytes
     return 2 * seql * batchsize * h_kv * headsize * kv_byte
         
     
 if __name__ == "__main__":
+    model_name = "OPT-30B"
+    ModelSpec(model_name)
     analyze()
-    # VERBOSE = True
-    VERBOSE = False
+    VERBOSE = True
+    # VERBOSE = False
+    PrintConfig()
     t = {"prefill":{}, "decode":{}}
     for stage in ['prefill', 'decode']:
         print("Stage:", stage)
@@ -194,21 +238,20 @@ if __name__ == "__main__":
         for op in Operations:
             if VERBOSE:
                 print('{')
-                print("operation: ", result[stage][op]["name"])
-                print("time: ", result[stage][op]["time"])
-                print("OPs: ", result[stage][op]["OPs"])
-                print("load_weight: ", result[stage][op]["load_weight"])
-                print("load_act: ", result[stage][op]["load_act"])
-                print("store_act: ", result[stage][op]["store_act"])
-                print("load_kv_cache: ", result[stage][op]["load_kv_cache"])
-                print("store_kv_cache: ", result[stage][op]["store_kv_cache"])
+                print("    operation: ", result[stage][op]["name"])
+                print("    time: ", result[stage][op]["time"])
+                # print("    OPs: ", result[stage][op]["OPs"])
+                # print("    load_weight: ", result[stage][op]["load_weight"])
+                # print("    load_act: ", result[stage][op]["load_act"])
+                # print("    store_act: ", result[stage][op]["store_act"])
+                # print("    load_kv_cache: ", result[stage][op]["load_kv_cache"])
+                # print("    store_kv_cache: ", result[stage][op]["store_kv_cache"])
                 print('}')
             t[stage] += result[stage][op]["time"]
         print("Total time: ", t[stage], "us")
     
-    kvsize = GetKVCacheSize()
+    kvsize = GetKVCacheSizePerLayer()
     print("KV Cache Size: ", kvsize / 1024 / 1024 / 1024, "GB")
 
-    MemoryBW = 200*1024*1024*1024 # 200GB/s
-    print("can prefetch ", t["decode"] * MemoryBW * 1e-6 / 1024 / 1024 / 1024, "GB")
-    
+    MemoryBW = 400*1024*1024*1024 # 400GB/s
+    print("can prefetch", t["decode"] * MemoryBW * 1e-6 / 1024 / 1024 / 1024, "GB every layer")
